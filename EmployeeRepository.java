ca073157d079880a219a261973581b5221fbe4b4
fingertechproject <74770417+fingertechproject@users.noreply.github.com>
2021-09-26 22:33:03 +0530

    Add files via upload

---
+++ EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.employee.EmployeeTask1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.employee.EmployeeTask1.entities.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
